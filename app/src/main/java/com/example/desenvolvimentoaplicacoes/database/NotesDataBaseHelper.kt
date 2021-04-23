c39c271476f09720ed4b2dad366cd37e654d7fe7
Raphaelamorimalex <67813638+Raphaelamorimalex@users.noreply.github.com>
2021-04-23 18:26:18 -0300

    Initial commit

---
+++ app/src/main/java/com/example/desenvolvimentoaplicacoes/database/NotesDataBaseHelper.kt
@@ -0,0 +1,28 @@
+package com.example.desenvolvimentoaplicacoes.database
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.provider.BaseColumns._ID
+
+class NotesDataBaseHelper(context: Context):SQLiteOpenHelper(context,"databaseNotes",null,1) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL("CREATE TABLE $TABLE_NOTES (" +
+                    "$_ID INTEGER NOT NULL PRIMARY KEY" +
+                    "$TITLE_NOTES TEXT NOT NULL"+
+                    "$DESCRIPTION_NOTE TEXT NOT NULL"
+
+                )
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+
+    companion object{
+        const val TABLE_NOTES : String = "Notes"
+        const val TITLE_NOTES: String = "title"
+        const val DESCRIPTION_NOTE: String = "description"
+    }
+
+}
